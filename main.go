7e18a3a612c8ebf15b2d71fa3c6188cebe276e05
Youyyyyyy <95914082+Youyyyyyy@users.noreply.github.com>
2023-01-20 06:09:19 +0800

    路由

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"assignment_bd/dao"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	// Init mysql
+	dao.Init()
+	r := gin.Default()
+	// 加载路由
+	initRouter(r)
+
+	//pprof
+	//pprof.Register(r)
+
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
--- router.go
+++ router.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter(r *gin.Engine) {
+	apiRouter := r.Group("/douyin")
+	// 基础
+	apiRouter.GET("/feed/", jwt.AuthWithoutLogin(), controller.Feed)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.GET("/user/", jwt.Auth(), controller.UserInfo)
+	apiRouter.POST("/publish/action/", jwt.AuthBody(), controller.Publish)
+	apiRouter.GET("/publish/list/", jwt.Auth(), controller.PublishList)
+	// 互动
+	apiRouter.POST("/favorite/action/", jwt.Auth(), controller.FavoriteAction)
+	apiRouter.GET("/favorite/list/", jwt.Auth(), controller.GetFavouriteList)
+	apiRouter.POST("/comment/action/", jwt.Auth(), controller.CommentAction)
+	apiRouter.GET("/comment/list/", jwt.AuthWithoutLogin(), controller.CommentList)
+	// 社交
+	apiRouter.POST("/relation/action/", jwt.Auth(), controller.RelationAction)
+	apiRouter.GET("/relation/follow/list/", jwt.Auth(), controller.GetFollowing)
+	apiRouter.GET("/relation/follower/list", jwt.Auth(), controller.GetFollowers)
+	apiRouter.GET("/relation/friend/list", jwt.Auth(), controller.GetFriends)
+}

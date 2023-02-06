0fabf3cb4061a83641a0dc8137d34c79a380c288
KM911 <2547715095@qq.com>
2023-02-06 15:00:46 +0800

    配置文件修改

--- config/default.go
+++ config/default.go
@@ -2,13 +2,12 @@ package config
 
 var (
 	// DSN 数据库地址
-	UseDSN = KM911LocalDSN
+	UseDSN = DSN_local
 	// Server 服务器地址 或者本机地址(需要端口号) 用于生成视频流url
 	Server = "http://192.168.1.5"
 	// 监听端口
 	Port = ":8888"
 
-	
 	DSN       = "root:1234567890@tcp(182.92.131.42:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	DSN_local = "root:Zsm@20020609@tcp(localhost:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	PankerDSN = "root:123456@tcp(127.0.0.1:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"

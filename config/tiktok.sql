807e5e32083bca03cca5184db8f2591192b061a7
KM911 <2547715095@qq.com>
2023-02-06 15:11:42 +0800

    数据库配置文件

--- config/tiktok.sql
+++ config/tiktok.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : panker
+ Source Server         : 本地数据库
  Source Server Type    : MySQL
- Source Server Version : 80022
+ Source Server Version : 80030
  Source Host           : localhost:3306
  Source Schema         : tiktok
 
  Target Server Type    : MySQL
- Target Server Version : 80022
+ Target Server Version : 80030
  File Encoding         : 65001
 
- Date: 29/01/2023 18:41:19
+ Date: 06/02/2023 15:11:31
 */
 
 SET NAMES utf8mb4;
@@ -22,105 +22,82 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '评论id',
-  `user_id` int NOT NULL COMMENT '评论者id',
-  `video_id` int NOT NULL COMMENT '视频id',
-  `action_type` int NOT NULL COMMENT '评论的状态（存在1，删除0）',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '评论id',
+  `user_id` int(0) NOT NULL COMMENT '评论者id',
+  `video_id` int(0) NOT NULL COMMENT '视频id',
+  `action_type` int(0) NOT NULL COMMENT '评论的状态（存在1，删除0）',
   `content` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '评论内容',
-  `created_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `created_at` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of comments
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for follows
 -- ----------------------------
 DROP TABLE IF EXISTS `follows`;
 CREATE TABLE `follows`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_id` int NOT NULL COMMENT '用户id',
-  `follower_id` int NOT NULL COMMENT '关注的用户的id',
-  `action_type` int NOT NULL COMMENT '关注的状态（关注1，取关0）',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(0) NOT NULL COMMENT '用户id',
+  `follower_id` int(0) NOT NULL COMMENT '关注的用户的id',
+  `action_type` int(0) NOT NULL COMMENT '关注的状态（关注1，取关0）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of follows
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for likes
 -- ----------------------------
 DROP TABLE IF EXISTS `likes`;
 CREATE TABLE `likes`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_id` int NOT NULL COMMENT '点赞的用户的id',
-  `video_id` int NOT NULL COMMENT '被点赞的视频的id',
-  `action_type` int NOT NULL COMMENT '赞的状态（点赞1，取消0）',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(0) NOT NULL COMMENT '点赞的用户的id',
+  `video_id` int(0) NOT NULL COMMENT '被点赞的视频的id',
+  `action_type` int(0) NOT NULL COMMENT '赞的状态（点赞1，取消0）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of likes
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for messages
 -- ----------------------------
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id主键',
-  `from_user_id` int NOT NULL COMMENT '发送消息的用户id',
-  `to_user_id` int NOT NULL COMMENT '接收消息的用户id',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id主键',
+  `from_user_id` int(0) NOT NULL COMMENT '发送消息的用户id',
+  `to_user_id` int(0) NOT NULL COMMENT '接收消息的用户id',
   `content` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '消息内容',
-  `create_time` datetime NOT NULL COMMENT '发送消息的时间',
+  `create_at` datetime(0) NOT NULL COMMENT '发送消息的时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of messages
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `username` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '用户名',
+  `id` int unsigned NOT NULL COMMENT '用户ID',
+  `username` varchar(32) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL COMMENT '用户名',
   `password` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '密码',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户的昵称',
   `salt` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '加密盐-生成密码用',
-  `create_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (2, 'panker', '123456', 'kkk', '0000-00-00 00:00:00');
-INSERT INTO `users` VALUES (3, 'test', 'test', '2333', '2023-01-29 11:27:14');
-INSERT INTO `users` VALUES (4, 'test', '123456', '2333', '2023-01-29 12:00:55');
-INSERT INTO `users` VALUES (5, 'yangsa', 'yangsa', '2333', '2023-01-29 12:35:10');
+  `create_at` datetime(0) NOT NULL COMMENT '创建时间',
+  `follow_count` int unsigned NOT NULL COMMENT '用户的关注数',
+  `follower_count` int unsigned NOT NULL COMMENT '用户的粉丝数',
+  PRIMARY KEY (`id`, `username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for videos
 -- ----------------------------
 DROP TABLE IF EXISTS `videos`;
 CREATE TABLE `videos`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '视频id',
-  `user_id` int NOT NULL COMMENT '作者id',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '视频id',
+  `user_id` int(0) NOT NULL COMMENT '作者id',
   `play_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '播放地址',
   `cover_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '封面地址',
-  `title` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '视频标题',
-  `created_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建实践',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '视频标题',
+  `created_at` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `favorite_count` int unsigned NOT NULL COMMENT '喜欢数目',
+  `comment_count` int unsigned NOT NULL COMMENT '评论数目',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
-
--- ----------------------------
--- Records of videos
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
